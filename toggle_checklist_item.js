962eb9ff4ea668efba0866c981c22e0301986a1e
Periklis Gkolias <PeriGK@users.noreply.github.com>
2018-06-30 01:10:02 +0300

    Update toggle_checklist_item.js

--- toggle_checklist_item.js
+++ toggle_checklist_item.js
@@ -2,7 +2,7 @@
 A script to toggle a checklist item, in a trello card
 This script is meant to work as follows:
 - Navigate to trello
-- Open the card you want to copy the items
+- Open the card you want to copy its items
 - Open the browser console(F12 usually)
 - Paste the script to the console tab
 - See that all the items are toggled
