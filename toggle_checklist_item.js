a99ad556cc4ad1e6156e852b1a998403eb8f7234
Periklis Gkolias <PeriGK@users.noreply.github.com>
2018-06-30 01:08:02 +0300

    Create toggle_checklist_item.js

---
+++ toggle_checklist_item.js
@@ -0,0 +1,15 @@
+/**
+A script to toggle a checklist item, in a trello card
+This script is meant to work as follows:
+- Navigate to trello
+- Open the card you want to copy the items
+- Open the browser console(F12 usually)
+- Paste the script to the console tab
+- See that all the items are toggled
+*/
+
+const checklist_items = document.getElementsByClassName('checklist-item-checkbox')
+const length = checklist_items.length
+for(let i = 0; i <length; i++) {
+  checklist_items[i].click()
+}
