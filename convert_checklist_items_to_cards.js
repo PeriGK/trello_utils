a09cef54bce97869fafa1605321b55b658783bff
Periklis Gkolias <PeriGK@users.noreply.github.com>
2018-06-30 01:02:54 +0300

    Update convert_checklist_items_to_cards.js

--- convert_checklist_items_to_cards.js
+++ convert_checklist_items_to_cards.js
@@ -1,9 +1,10 @@
-/** This script is meant to work as follows:
+/** Convert all the items of a checklist to cards vi the clipboard and copy-paste.
+This script is meant to work as follows:
 - Navigate to trello
 - Open the card you want to copy the items
 - Open the browser console(F12 usually)
 - Paste the script
-- Create a new card and paste them.
+- Click to create a new card and paste them.
 - Trello will prompt you to create N new cards
 */
 copy($(".checklist-item:not(.checklist-item-checked)").map(function() {
